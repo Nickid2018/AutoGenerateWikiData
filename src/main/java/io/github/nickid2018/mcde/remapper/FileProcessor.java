7c611303018f4fd069f1964456f5803f22b8e1b9
Nickid2018 <nickid2018@outlook.com>
2023-12-24 15:51:07 +0800

    Revert "no light update"
    This reverts commit e5718bd2be5646771118f970b68852aed6590278.

--- src/main/java/io/github/nickid2018/mcde/remapper/FileProcessor.java
+++ src/main/java/io/github/nickid2018/mcde/remapper/FileProcessor.java
@@ -288,8 +288,20 @@ public class FileProcessor {
                         method.instructions = list;
                         injectedCount++;
                     }
+                    if (method.name.equals(Constants.INJECT_THREADED_LEVEL_LIGHT_ENGINE_METHOD2) &&
+                            method.desc.equals(Constants.INJECT_THREADED_LEVEL_LIGHT_ENGINE_METHOD2_DESC)) {
+                        InsnList list = new InsnList();
+                        LabelNode jumpBack = new LabelNode();
+                        list.add(jumpBack);
+                        list.add(new MethodInsnNode(Opcodes.INVOKESTATIC,
+                                "java/lang/Thread", "yield", "()V", false));
+                        list.add(new JumpInsnNode(Opcodes.GOTO, jumpBack));
+                        list.add(method.instructions);
+                        method.instructions = list;
+                        injectedCount++;
+                    }
                 }
-                if (injectedCount != 1)
+                if (injectedCount != 2)
                     throw new RuntimeException("Failed to inject!");
                 ClassWriter writer = new ClassWriter(0);
                 node.accept(writer);
