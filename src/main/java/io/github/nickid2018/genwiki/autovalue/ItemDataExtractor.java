506a255410c8996010b1ff3715ca5a05b91631f7
Leo-768 <leo768leo768@gmail.com>
2024-01-27 00:33:51 +0800

    Add Attribute Modifiers

--- src/main/java/io/github/nickid2018/genwiki/autovalue/ItemDataExtractor.java
+++ src/main/java/io/github/nickid2018/genwiki/autovalue/ItemDataExtractor.java
@@ -4,6 +4,7 @@ import io.github.nickid2018.genwiki.autovalue.wikidata.*;
 import io.github.nickid2018.genwiki.inject.InjectedProcess;
 import io.github.nickid2018.genwiki.inject.SourceClass;
 import lombok.SneakyThrows;
+import com.google.common.collect.Multimap;
 
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
@@ -17,15 +18,20 @@ public class ItemDataExtractor {
     private static final Class<?> RARITY_CLASS;
     private static final Class<?> CREATIVE_MODE_TABS_CLASS;
     private static final Class<?> FOOD_PROPERTIES_CLASS;
+    private static final Class<?> EQUIPMENT_SLOT_CLASS;
 
     private static final MethodHandle ITEM_GET_MAX_STACK_SIZE;
     private static final MethodHandle ITEM_STACK_GET_ITEM;
     private static final MethodHandle ITEM_GET_MAX_DAMAGE;
     private static final MethodHandle ITEM_GET_FOOD_PROPERTIES;
+    private static final MethodHandle ITEM_GET_ATTRIBUTE_MODIFIERS;
     private static final MethodHandle FOOD_PROPERTIES_NUTRITION;
     private static final MethodHandle FOOD_PROPERTIES_SATURATION_MODIFIER;
     private static final MethodHandle BUILD_TAB_CONTENTS;
+    private static final MethodHandle ATTRIBUTE_MODIFIER_GET_AMOUNT;
+    private static final MethodHandle ATTRIBUTE_MODIFIER_GET_OPERATION;
     private static final MethodHandle GET_FUEL;
+    private static final MethodHandle STRING_REPRESENTABLE_GET_SERIALIZED_NAME;
 
     private static final VarHandle ITEM_RARITY;
     private static final VarHandle CREATIVE_MODE_TAB_DISPLAY_ITEMS;
@@ -37,12 +43,17 @@ public class ItemDataExtractor {
             RARITY_CLASS = Class.forName("net.minecraft.world.item.Rarity");
             CREATIVE_MODE_TABS_CLASS = Class.forName("net.minecraft.world.item.CreativeModeTabs");
             FOOD_PROPERTIES_CLASS = Class.forName("net.minecraft.world.food.FoodProperties");
+            EQUIPMENT_SLOT_CLASS = Class.forName("net.minecraft.world.entity.EquipmentSlot");
             Class<?> creativeModeTabClass = Class.forName("net.minecraft.world.item.CreativeModeTab");
             ITEM_GET_MAX_STACK_SIZE = lookup.unreflect(ITEM_CLASS.getMethod("getMaxStackSize"));
             ITEM_GET_MAX_DAMAGE = lookup.unreflect(ITEM_CLASS.getMethod("getMaxDamage"));
             ITEM_GET_FOOD_PROPERTIES = lookup.unreflect(ITEM_CLASS.getMethod("getFoodProperties"));
+            ITEM_GET_ATTRIBUTE_MODIFIERS = lookup.unreflect(ITEM_CLASS.getMethod("getDefaultAttributeModifiers", EQUIPMENT_SLOT_CLASS));
             FOOD_PROPERTIES_NUTRITION = lookup.unreflect(FOOD_PROPERTIES_CLASS.getMethod("getNutrition"));
             FOOD_PROPERTIES_SATURATION_MODIFIER = lookup.unreflect(FOOD_PROPERTIES_CLASS.getMethod("getSaturationModifier"));
+            Class<?> attributeModifier = Class.forName("net.minecraft.world.entity.ai.attributes.AttributeModifier");
+            ATTRIBUTE_MODIFIER_GET_AMOUNT = lookup.unreflect(attributeModifier.getMethod("getAmount"));
+            ATTRIBUTE_MODIFIER_GET_OPERATION = lookup.unreflect(attributeModifier.getMethod("getOperation"));
             MethodHandles.Lookup privateLookup = MethodHandles.privateLookupIn(ITEM_CLASS, lookup);
             ITEM_RARITY = privateLookup.findVarHandle(ITEM_CLASS, "rarity", RARITY_CLASS);
             MethodHandles.Lookup privateLookup2 = MethodHandles.privateLookupIn(creativeModeTabClass, lookup);
@@ -54,6 +65,8 @@ public class ItemDataExtractor {
                     Class.forName("net.minecraft.core.HolderLookup$Provider")));
             GET_FUEL = lookup.unreflect(
                     Class.forName("net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity").getMethod("getFuel"));
+            STRING_REPRESENTABLE_GET_SERIALIZED_NAME = lookup.unreflect(
+                    Class.forName("net.minecraft.util.StringRepresentable").getMethod("getSerializedName"));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -65,6 +78,7 @@ public class ItemDataExtractor {
     private static final NumberWikiData BURN_DURATION = new NumberWikiData().setFallback(0);
     private static final NumberWikiData MAX_DAMAGE = new NumberWikiData().setFallback(0);
     private static final DoubleNumberWikiData FOOD_PROPERTIES = new DoubleNumberWikiData().setFallback(0, 0).setFallbackNil(true);
+    private static final AttributeModifiersData ATTRIBUTE_MODIFIERS = new AttributeModifiersData();
 
     @SneakyThrows
     @SuppressWarnings("unchecked")
@@ -87,6 +101,19 @@ public class ItemDataExtractor {
             MAX_STACK_SIZE.put(itemID, maxStackSize);
             int maxDamage = (int) ITEM_GET_MAX_DAMAGE.invoke(item);
             MAX_DAMAGE.put(itemID, maxDamage);
+            for (@SourceClass("EquipmentSlot") Object slot : EQUIPMENT_SLOT_CLASS.getEnumConstants()) {
+                @SourceClass("Multimap<Holder<Attribute>, AttributeModifier>")
+                Multimap<Object, Object> attributeModifiers = (Multimap<Object, Object>) ITEM_GET_ATTRIBUTE_MODIFIERS.invoke(item, slot);
+                if (attributeModifiers.isEmpty()) continue;
+                String slotID = (String) STRING_REPRESENTABLE_GET_SERIALIZED_NAME.invoke(slot);
+                for (Object attribute : attributeModifiers.keys()){
+                    String attributeID = InjectedProcess.holderToString(attribute);
+                    for (Object attributeModifier : attributeModifiers.get(attribute)){
+                        String operation = (String) STRING_REPRESENTABLE_GET_SERIALIZED_NAME.invoke(ATTRIBUTE_MODIFIER_GET_OPERATION.invoke(attributeModifier));
+                        ATTRIBUTE_MODIFIERS.add(itemID, attributeID, slotID, (Double) ATTRIBUTE_MODIFIER_GET_AMOUNT.invoke(attributeModifier), operation);
+                    }
+                }
+            }
             @SourceClass("Rarity")
             Object rarity = ITEM_RARITY.get(item);
             String rarityName = (String) InjectedProcess.ENUM_NAME.invoke(rarity);
@@ -147,5 +174,6 @@ public class ItemDataExtractor {
         WikiData.write(BURN_DURATION, "item_burn_duration.txt");
         WikiData.write(MAX_DAMAGE, "item_max_damage.txt");
         WikiData.write(FOOD_PROPERTIES, "item_food_properties.txt");
+        WikiData.write(ATTRIBUTE_MODIFIERS, "item_attribute_modifiers.txt");
     }
 }
