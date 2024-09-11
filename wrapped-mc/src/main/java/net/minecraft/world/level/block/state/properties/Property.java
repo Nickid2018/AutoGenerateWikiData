42d1898e5ce485930747ef8c0c121f671801de7b
nickid2018 <nickid2018@outlook.com>
2024-09-12 00:31:13 +0800

    24w37a: Property Collection->List

--- wrapped-mc/src/main/java/net/minecraft/world/level/block/state/properties/Property.java
+++ wrapped-mc/src/main/java/net/minecraft/world/level/block/state/properties/Property.java
@@ -1,10 +1,10 @@
 package net.minecraft.world.level.block.state.properties;
 
-import java.util.Collection;
+import java.util.List;
 
 public abstract class Property<T extends Comparable<T>> {
 
-    public abstract Collection<T> getPossibleValues();
+    public abstract List<T> getPossibleValues();
 
     public String getName() {
         throw new RuntimeException();
